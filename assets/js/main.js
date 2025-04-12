f5e00db5d94103e40f2a710e25dc0e3372fccd2d
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2025-04-12 07:28:30 +0300

    feat: add initial login and index pages
    - Created basic login page structure
    - Added main index page
    - Initial project setup for version control

---
+++ assets/js/main.js
@@ -0,0 +1,161 @@
+const container = document.getElementById("crypto-container");
+const searchInput = document.getElementById("search-input");
+const filterButtons = document.querySelectorAll(".filter-btn");
+
+let allCoins = [];
+
+// Fetching API
+const fetchCryptoData = async () => {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1"
+    );
+    const data = await response.json();
+    console.log("Data Updated", data);
+    allCoins = data;
+    displayCrypto(data);
+  } catch (error) {}
+};
+
+// We are adding an interval to update the data every 30 seconds.
+setInterval(fetchCryptoData, 30000);
+
+// Display Card
+const displayCrypto = (coins) => {
+  container.innerHTML = coins
+    .map((coin) => {
+      const priceChange = coin.price_change_percentage_24h.toFixed(2);
+      const changeClass = priceChange >= 0 ? "text-success" : "text-danger";
+
+      return `
+      <div class="col-md-4">
+      <div class="card">
+        <img src="${coin.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${
+            coin.name
+          } (${coin.symbol.toUpperCase()})</h5>
+          <p class="card-text fw-semibold">Fiyat: $${coin.current_price}</p>
+          <p class="card-text ${changeClass} fw-semibold">24h: %${priceChange}</p>
+          <button class="btn btn-outline-dark" onclick="showChart('${
+            coin.id
+          }', '${coin.name}', '${
+        coin.price_change_percentage_24h
+      }')" >Grafiği Göster</button>
+      <button class="btn btn-warning" onclick="toggleFavorite('${
+        coin.id
+      }')">⭐ Favorilere Ekle</button>
+      </div>
+      </div>
+      </div>
+    `;
+    })
+    .join("");
+};
+
+// Search Filter By Name and By Symbol
+searchInput.addEventListener("input", () => {
+  const term = searchInput.value.toLowerCase();
+  const filteredData = allCoins.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(term) ||
+      coin.symbol.toLowerCase().includes(term)
+  );
+  displayCrypto(filteredData);
+});
+
+// Filter Button Click Event
+filterButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const type = btn.dataset.filter;
+    let filteredData = [...allCoins];
+
+    if (type === "favorites") {
+      // Get the favorite coins from localStorage
+      const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+      filteredData = allCoins.filter((coin) => favorites.includes(coin.id));
+    } else if (type === "gainers") {
+      filteredData = allCoins.filter(
+        (coin) => coin.price_change_percentage_24h >= 0
+      );
+    } else if (type === "losers") {
+      filteredData = allCoins.filter(
+        (coin) => coin.price_change_percentage_24h < 0
+      );
+    }
+
+    displayCrypto(filteredData);
+  });
+});
+
+// Chart Section
+let chart;
+
+const showChart = async (coinId, coinName, priceChange) => {
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=7`
+  );
+  const data = await response.json();
+  console.log(data);
+  const labels = data.prices.map((p) => new Date(p[0]).toLocaleDateString());
+  console.log(labels);
+  const prices = data.prices.map((price) => price[1]);
+  console.log(prices);
+
+  const ctx = document.getElementById("priceChart").getContext("2d");
+
+  if (chart) {
+    chart.destroy();
+  }
+
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: `${coinName} - Son 7 Gün`,
+          data: prices,
+          borderColor: priceChange >= 0 ? "green" : "red",
+          backgroundColor:
+            priceChange >= 0 ? "rgba(0, 128, 0, 0.2)" : "rgba(255, 0, 0, 0.2)",
+          fill: true,
+          tension: 0.4,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          ticks: {
+            color: "#000",
+          },
+        },
+        x: {
+          ticks: {
+            color: "#000",
+          },
+        },
+      },
+    },
+  });
+
+  const modal = new bootstrap.Modal(document.getElementById("chartModal"));
+  modal.show();
+};
+
+fetchCryptoData();
+
+// Favorite Coin
+const toggleFavorite = (coinId) => {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  if (favorites.includes(coinId)) {
+    favorites = favorites.filter((id) => id !== coinId);
+  } else {
+    favorites.push(coinId);
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+};
